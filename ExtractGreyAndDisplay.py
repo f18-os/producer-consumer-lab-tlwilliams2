128b4989785333340bacdee400af29a161b0a977
student <tlwilliams2@miners.utep.edu>
2018-11-18 23:58:35 -0700

    Lab4

---
+++ ExtractGreyAndDisplay.py
@@ -0,0 +1,123 @@
+import threading
+import cv2
+import numpy as np
+import base64
+import queue
+
+def extractFrames(fileName, outputBuffer):
+    # Initialize frame count 
+    count = 0
+
+    # open video file
+    vidcap = cv2.VideoCapture(fileName)
+
+    # read first image
+    success,image = vidcap.read()
+    
+    print("Reading frame {} {} ".format(count, success))
+    while success:
+        # get a jpg encoded frame
+        success, jpgImage = cv2.imencode('.jpg', image)
+
+        #encode the frame as base 64 to make debugging easier
+        jpgAsText = base64.b64encode(jpgImage)
+
+        # add the frame to the buffer
+        outputBuffer.put(jpgAsText)
+       
+        success,image = vidcap.read()
+        print('Reading frame {} {}'.format(count, success))
+        count += 1
+
+    print("Frame extraction complete")
+    
+def greyFrames(inputBuffer,outputBuffer):
+    # initialize frame count
+    count = 0
+
+    # get the next frame file name
+    #inFileName = "{}/frame_{:04d}.jpg".format(outputDir, count)
+
+    # load the next file
+    #inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
+
+    while not inputBuffer.empty():
+        print("Converting frame {}".format(count))
+        
+        frameAsText = inputBuffer.get()
+        
+        # decode the frame 
+        jpgRawImage = base64.b64decode(frameAsText)
+
+        # convert the raw frame to a numpy array
+        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+
+        # convert the image to grayscale
+        grayscaleFrame = cv2.cvtColor(jpgImage, cv2.COLOR_BGR2GRAY)
+    
+        # generate output file name
+        #outFileName = "{}/grayscale_{:04d}.jpg".format(outputDir, count)
+
+        # write output file
+        #cv2.imwrite(outFileName, grayscaleFrame)
+        
+        outputBuffer.put(grayscaleFrame)
+
+        count += 1
+
+        # generate input file name for the next frame
+        #inFileName = "{}/frame_{:04d}.jpg".format(outputDir, count)
+
+        # load the next frame
+        #inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
+
+
+
+def displayFrames(inputBuffer):
+    # initialize frame count
+    count = 0
+
+    # go through each frame in the buffer until the buffer is empty
+    while not inputBuffer.empty():
+        # get the next frame
+        frameAsText = inputBuffer.get()
+
+        # decode the frame 
+        jpgRawImage = base64.b64decode(frameAsText)
+
+        # convert the raw frame to a numpy array
+        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+        
+        # get a jpg encoded frame
+        img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+
+        print("Displaying frame {}".format(count))        
+
+        # display the image in a window called "video" and wait 42ms
+        # before displaying the next frame
+        cv2.imshow("Video", img)
+        if cv2.waitKey(42) and 0xFF == ord("q"):
+            break
+
+        count += 1
+
+    print("Finished displaying all frames")
+    # cleanup the windows
+    cv2.destroyAllWindows()
+
+# filename of clip to load
+filename = 'clip.mp4'
+
+# shared queue  
+extractionQueue = queue.Queue()
+
+helperExtractionQueue = queue.Queue()
+
+# extract the frames
+extractFrames(filename,extractionQueue)
+
+greyFrames(extractionQueue,helperExtractionQueue)
+
+# display the frames
+displayFrames(helperExtractionQueue)
+
